2a98f3ecb315f59207161e0735c54111da21d2bd
Adam Harris <adam@omnified.io>
2023-07-30 16:20:19 +0200

    Added some helix config

--- modules/home-manager/helix.nix
+++ modules/home-manager/helix.nix
@@ -12,5 +12,18 @@
         auto-format = true;
       }];
     };
+    settings = {
+      theme = "base16";
+      editor = {
+        line-number = "relative";
+        lsp.display-messages = true;
+      };
+      keys.normal = {
+        space.space = "file_picker";
+        space.w = ":w";
+        space.q = ":q";
+        esc = [ "collapse_selection" "keep_primary_selection" ];
+      };
+    };
   };
 }
