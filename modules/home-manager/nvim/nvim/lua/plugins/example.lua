6383c553d8141c110bd62c0d01f118c35cab70d0
Adam Harris <adam@omnified.io>
2023-10-03 13:22:02 +0200

    set up rust lsp

--- modules/home-manager/nvim/nvim/lua/plugins/example.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/example.lua
@@ -41,12 +41,27 @@ return {
     -- override nvim-cmp and add cmp-emoji
     {
         "hrsh7th/nvim-cmp",
-        dependencies = { "hrsh7th/cmp-emoji" },
+        dependencies = {
+            "hrsh7th/cmp-emoji",
+            {
+                "Saecki/crates.nvim",
+                event = { "BufRead Cargo.toml" },
+                config = true,
+            },
+        },
         ---@param opts cmp.ConfigSchema
         opts = function(_, opts)
             local cmp = require("cmp")
-            opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+            opts.sources =
+                cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" }, { name = "crates" } }))
         end,
+    },
+
+    -- for rust
+    {
+        "Saecki/crates.nvim",
+        event = { "BufRead Cargo.toml" },
+        config = true,
     },
 
     -- change some telescope options and a keymap to browse plugin files
@@ -85,17 +100,17 @@ return {
     },
 
     -- add pyright to lspconfig
-    {
-        "neovim/nvim-lspconfig",
-        ---@class PluginLspOpts
-        opts = {
-            ---@type lspconfig.options
-            servers = {
-                -- pyright will be automatically installed with mason and loaded with lspconfig
-                pyright = {},
-            },
-        },
-    },
+    -- {
+    --   "neovim/nvim-lspconfig",
+    --   ---@class PluginLspOpts
+    --   opts = {
+    --     ---@type lspconfig.options
+    --     servers = {
+    --       -- pyright will be automatically installed with mason and loaded with lspconfig
+    --       pyright = {},
+    --     },
+    --   },
+    -- },
 
     -- add tsserver and setup with typescript.nvim instead of lspconfig
     {
@@ -132,6 +147,82 @@ return {
         },
     },
 
+    -- for rust
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                -- Ensure mason installs the server
+                rust_analyzer = {
+                    keys = {
+                        { "K", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+                        { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+                        { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+                    },
+                    settings = {
+                        ["rust-analyzer"] = {
+                            cargo = {
+                                allFeatures = true,
+                                loadOutDirsFromCheck = true,
+                                runBuildScripts = true,
+                            },
+                            -- Add clippy lints for Rust.
+                            checkOnSave = {
+                                allFeatures = true,
+                                command = "clippy",
+                                extraArgs = { "--no-deps" },
+                            },
+                            procMacro = {
+                                enable = true,
+                                ignored = {
+                                    ["async-trait"] = { "async_trait" },
+                                    ["napi-derive"] = { "napi" },
+                                    ["async-recursion"] = { "async_recursion" },
+                                },
+                            },
+                        },
+                    },
+                },
+                taplo = {
+                    keys = {
+                        {
+                            "K",
+                            function()
+                                if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+                                    require("crates").show_popup()
+                                else
+                                    vim.lsp.buf.hover()
+                                end
+                            end,
+                            desc = "Show Crate Documentation",
+                        },
+                    },
+                },
+            },
+            setup = {
+                rust_analyzer = function(_, opts)
+                    local rust_tools_opts = require("lazyvim.util").opts("rust-tools.nvim")
+                    require("rust-tools").setup(vim.tbl_deep_extend("force", rust_tools_opts or {}, { server = opts }))
+                    return true
+                end,
+            },
+        },
+    },
+    {
+        "nvim-neotest/neotest",
+        optional = true,
+        dependencies = {
+            "rouge8/neotest-rust",
+        },
+        opts = {
+            adapters = {
+                ["neotest-rust"] = {},
+            },
+        },
+    },
+    {
+        "rouge8/neotest-rust",
+    },
     -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
     -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
     { import = "lazyvim.plugins.extras.lang.typescript" },
@@ -167,8 +258,23 @@ return {
         opts = function(_, opts)
             -- add tsx and treesitter
             vim.list_extend(opts.ensure_installed, {
+                "bash",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "ron",
+                "rust",
+                "toml",
                 "tsx",
                 "typescript",
+                "vim",
+                "yaml",
             })
         end,
     },
@@ -208,6 +314,7 @@ return {
                 "shellcheck",
                 "shfmt",
                 "flake8",
+                "codelldb",
             },
         },
     },
@@ -263,5 +370,45 @@ return {
                 end, { "i", "s" }),
             })
         end,
+    },
+    {
+        "simrat39/rust-tools.nvim",
+        lazy = true,
+        opts = function()
+            local ok, mason_registry = pcall(require, "mason-registry")
+            local adapter ---@type any
+            if ok then
+                -- rust tools configuration for debugging support
+                local codelldb = mason_registry.get_package("codelldb")
+                local extension_path = codelldb:get_install_path() .. "/extension/"
+                local codelldb_path = extension_path .. "adapter/codelldb"
+                local liblldb_path = ""
+                if vim.loop.os_uname().sysname:find("Windows") then
+                    liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+                elseif vim.fn.has("mac") == 1 then
+                    liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+                else
+                    liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+                end
+                adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+            end
+            return {
+                dap = {
+                    adapter = adapter,
+                },
+                tools = {
+                    on_initialized = function()
+                        vim.cmd([[
+                augroup RustLSP
+                  autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+                  autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+                  autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+                augroup END
+              ]])
+                    end,
+                },
+            }
+        end,
+        config = function() end,
     },
 }
